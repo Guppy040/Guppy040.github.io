bc86d4f9066743c28875d13a2ef8ee0b6dd971d1
Guppy040 <m011816065@edu.teu.ac.jp>
2019-10-16 18:15:29 +0900

    bootstrap作成

--- 191016_2/function-test.js
+++ 191016_2/function-test.js
@@ -1,3 +1,17 @@
 function testfunction(){
     console.log("testFunctionを実行");
 }
+
+function aiueo(hikisuu1 ,hikisuu2, hikisuu3){
+    console.log('hikisuu1: ${hikisuu1}');
+    console.log('hikisuu2: ${hikisuu2}');
+    console.log('hikisuu3: ${hikisuu3}');
+
+}
+aiueo(1,2,3);
+
+function kakikukeko(hikisuu1 = "デフォルト"){
+    console.log('hikisuu1: ${hikisuu1}');
+}
+kakikukeko();
+kakikukeko("カスタマイズした値");

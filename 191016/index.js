b29111e923e3af5fd7adfcdd28a50775a86fc6e0
Guppy040 <m011816065@edu.teu.ac.jp>
2019-10-16 17:18:28 +0900

    bootstrap作成

--- 191016/index.js
+++ 191016/index.js
@@ -1,1 +1,13 @@
-console.log("読み込み成功")
+let variableA;
+variableA = "最初の代入A";
+console.log(variableA);
+
+let variableB = "最初の代入B";
+console.log(variableB);
+
+variableA = "二回目の代入A";
+console.log(variableA);
+
+variableA = variableB;
+console.log(variableA);
+

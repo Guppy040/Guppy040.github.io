f62ccc9bd45cceda827c84eaf539aed8252e77e2
Guppy040 <m011816065@edu.teu.ac.jp>
2019-10-30 17:58:16 +0900

    bootstrap作成

---
+++ 191030/modal.js
@@ -0,0 +1,6 @@
+function showModal() {
+    console.log("showModal() called");
+    // div#modalの中に文字列を表示する
+    let modal = document.querySelector("div#modal");
+    modal.innerHTML = "クリックされたよ！";
+}
---
+++ 191030_3/modal.js
@@ -0,0 +1,11 @@
+function appendCircle(){
+    console.log("appendCircle() called");
+    let modal = document.querySelector("div#modal");
+    modal.append("○");
+}
+
+function appendCross(){
+    console.log("appendCross() called");
+    let modal = document.querySelector("div#modal");
+    modal.append("☓");
+}

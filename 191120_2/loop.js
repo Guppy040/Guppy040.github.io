927383f4b9c918ee8b254487aef41693f1f6f34c
Guppy040 <m011816065@edu.teu.ac.jp>
2019-11-20 18:21:16 +0900

    bootstrap作成

--- 191120_2/loop.js
+++ 191120_2/loop.js
@@ -1,5 +1,18 @@
-let num = 0;
-while(num < 10000){
-    num++;
+console.log("whileの練習");
 
+function doLoop(){
+    console.log("実行ボタンが押された");
+    let i = 0;
+    let sum = 0;
+    while(i <= 10000){
+        sum = sum + i;
+
+        if(sum >= 50000){
+            break;
+        }
+        i++;
+    }
+
+    let answer = document.querySelector("#answer");
+    answer.innerHTML = sum;
 }

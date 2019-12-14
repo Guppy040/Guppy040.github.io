2d3681382188f49d823bd5e6d122f79ba1f1f866
Guppy040 <m011816065@edu.teu.ac.jp>
2019-12-15 01:48:45 +0900

    bootstrap作成

--- tyuukann_kadai/index.js
+++ tyuukann_kadai/index.js
@@ -1,4 +1,51 @@
-function OnButtonClick() {
-    target = document.getElementById("output");
-    target.innerHTML = "Penguin";
+console.log(tweets);
+
+
+
+
+
+
+let answer = document.querySelector("#answer");
+
+
+let answerHTML = "";
+
+/**
+ * 構築するHTMLのli要素の例
+ * <li><b>太郎</b>: 満腹度が回復した <i>2018-11-01 20:05:38</i></li>
+ */
+answerHTML = "<ul>";
+let i = 0;
+while (i < tweets.length) {
+    answerHTML = answerHTML +
+        tweets[i].image+ "<li>"
+        + "<b>" + tweets[i].name + "</b>: "
+        + tweets[i].message + " "
+        + "<i>" + tweets[i].tweetedAt + "</i>"
+        + "</li>" +"<hr>";
+    i++;
+}
+
+
+answerHTML = answerHTML + "</ul>";
+
+answer.innerHTML = answerHTML;
+
+function tarou(){
+    console.log("太郎が押された")
+    answerHTML ="<ul>"
+    i=0;
+
+    while(i<taroulength) {
+        answerHTML = answerHTML +
+            tweets[i].image + "<li>"
+            + "<b>" + taroutweets[i].name + "</b>: "
+            + taroutweets[i].message + " "
+            + "<i>" + taroutweets[i].tweetedAt + "</i>"
+            + "</li>" + "<hr>";
+        i++;
+
+    }
+
+
 }
